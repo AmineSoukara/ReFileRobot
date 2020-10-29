d53cf655916e4401229b20688eb9aa4f6871cb27
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-10-29 21:22:41 +0100

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -83,7 +83,7 @@ async def rename_video(bot, update):
             new_file_name = download_location + file_name
             os.rename(the_real_download_location, new_file_name)
             await bot.edit_message_text(
-                text=Translation.UPLOAD_START,
+                text=Translation.UPLOAD_START_VIDEO,
                 chat_id=update.chat.id,
                 message_id=b.message_id
                 )
@@ -132,7 +132,7 @@ async def rename_video(bot, update):
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
-                    Translation.UPLOAD_START,
+                    Translation.UPLOAD_START_VIDEO,
                     b, 
                     c_time
                 )
